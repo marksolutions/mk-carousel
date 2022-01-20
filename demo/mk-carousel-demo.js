749e9d99f6090ff5a1bb99b1e5a1103afa36fbdb
Hiten Bhayani <hitenb@codetailor.in>
2022-01-20 17:34:21 +0530

    feat: retro spec

--- demo/mk-carousel-demo.js
+++ demo/mk-carousel-demo.js
@@ -1,12 +1,20 @@
-import {LitElement, html} from 'lit-element';
+import { html } from "lit-element";
 
-import "../mk-carousel.js";
+import { MkCarousel } from "../mk-carousel.js";
 
-class MkCarouselDemo extends LitElement {
-  render() {
+class MkCarouselDemo extends MkCarousel {
+  get _getContentTemplate() {
     return html`
-      <mk-carousel></mk-carousel>
+      <div class="slide">
+        <img src="img/image1.jpg" alt="" />
+      </div>
+      <div class="slide">
+        <img src="img/image2.jpg" alt="" />
+      </div>
+      <div class="slide">
+        <img src="img/image3.jpg" alt="" />
+      </div>
     `;
   }
 }
-customElements.define('mk-carousel-demo', MkCarouselDemo);
+customElements.define("mk-carousel-demo", MkCarouselDemo);
--- mk-carousel.js
+++ mk-carousel.js
@@ -1,22 +1,45 @@
 import { LitElement, html, css } from "lit-element";
 
-class MkCarousel extends LitElement {
 
-  static get styles(){
+/**
+ * Behaviour
+ * - Auto scroll
+ * - Navigator on left and right.
+ *  - left is invisible when first item is visible
+ *  - right is invisible when last item is visible
+ * - indicator
+ *  - depends on number of items, should be divided by number of items shows on screen.
+ */
+
+export class MkCarousel extends LitElement {
+  static get styles() {
     return [
       css`
-        :host{
+        :host {
           display: block;
           min-height: 100vh;
+          margin: 0;
         }
-      `
-    ]
+
+        .slide{
+          position: absolute;
+          top: 0;
+          bottom: 0;
+        }
+      `,
+    ];
   }
 
   render() {
     return html`
-      Mk - carousel
+      <div class="container">
+        <div class="track-container">${this._getContentTemplate}</div>
+      </div>
     `;
+  }
+
+  get _getContentTemplate() {
+    return html`<slot></slot>`;
   }
 }
 customElements.define("mk-carousel", MkCarousel);
