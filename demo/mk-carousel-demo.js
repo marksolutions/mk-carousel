88ae32a89beb18a17727e16eb941988e7df629b1
Hiten Bhayani <hitenb@codetailor.in>
2022-01-21 18:03:37 +0530

    feat: integrate splidejs

--- demo/mk-carousel-demo.js
+++ demo/mk-carousel-demo.js
@@ -1,19 +1,51 @@
-import { html } from "lit-element";
+import { html, css } from "lit-element";
 
 import { MkCarousel } from "../mk-carousel.js";
 
 class MkCarouselDemo extends MkCarousel {
+  static get styles() {
+    return [
+      MkCarousel.styles,
+      css`
+        img {
+          width: 100%;
+        }
+      `,
+    ];
+  }
+
+  constructor(){
+    super();
+    this.type = 'slide';
+    this.rewind = true;
+    this.perPage = 1;
+    this.autoPlay = true;
+    this.interval = 3000;
+  }
+
   get _getContentTemplate() {
     return html`
-      <div class="slide">
-        <img src="img/image1.jpg" alt="" />
-      </div>
-      <div class="slide">
-        <img src="img/image2.jpg" alt="" />
-      </div>
-      <div class="slide">
-        <img src="img/image3.jpg" alt="" />
-      </div>
+      <li class="splide__slide">
+        <img src="img/image1.jpg" />
+      </li>
+      <li class="splide__slide">
+        <img src="img/image2.jpg" />
+      </li>
+      <li class="splide__slide">
+        <img src="img/image3.jpg" />
+      </li>
+      <li class="splide__slide">
+        <img src="img/image4.jpg" />
+      </li>
+      <li class="splide__slide">
+        <img src="img/image5.jpg" />
+      </li>
+      <li class="splide__slide">
+        <img src="img/image6.jpg" />
+      </li>
+      <li class="splide__slide">
+        <img src="img/image7.jpg" />
+      </li>
     `;
   }
 }
--- mk-carousel.js
+++ mk-carousel.js
@@ -1,41 +1,121 @@
 import { LitElement, html, css } from "lit-element";
-
+import { defaultStyle, skyblueStyle, seablueStyle } from "./style";
+import Splide from "@splidejs/splide";
 
 /**
  * Behaviour
- * - Auto scroll
- * - Navigator on left and right.
- *  - left is invisible when first item is visible
- *  - right is invisible when last item is visible
- * - indicator
+ * - AutoPlay (Boolean)
+ * - HasNavigator (Boolean)
+ *  - Navigator on left and right.
+ *   - left is invisible when first item is visible
+ *   - right is invisible when last item is visible
+ * - HasIndicator (Boolean)
  *  - depends on number of items, should be divided by number of items shows on screen.
  */
 
 export class MkCarousel extends LitElement {
   static get styles() {
     return [
+      defaultStyle,
       css`
         :host {
           display: block;
-          min-height: 100vh;
-          margin: 0;
-        }
-
-        .slide{
-          position: absolute;
-          top: 0;
-          bottom: 0;
         }
       `,
     ];
   }
 
+  static get properties() {
+    return {
+      /**
+       * Type of Carousel, possible values - slide(default), loop, fade
+       */
+      type: {
+        type: String,
+        reflect: true,
+      },
+
+      /**
+       * Determines whether to rewind the slider or not. This does not work in the loop mode. Default false.
+       */
+      rewind: {
+        type: Boolean,
+        reflect: true,
+      },
+
+      /**
+       * Determines the number of slides to display in a page. The value must be an integer. Default 1.
+       */
+      perPage: {
+        type: Number,
+      },
+
+      /**
+       * Boolean, auto play slider when property is true.
+       */
+      autoPlay: {
+        type: Boolean,
+        reflect: true,
+      },
+
+      /**
+       * The autoplay interval duration in milliseconds, default 5000. Use the `data-splide-interval` attribute to override this value for the specific slide.
+       * e.g. 
+       * <li class="splide__slide" data-splide-interval="1000"></li>
+       * <li class="splide__slide" data-splide-interval="10000"></li>
+       */
+      interval: {
+        type: Number
+      },
+
+      /**
+       * shows navigator at left and right side when property is true.
+       */
+      hasNavigator: {
+        type: Boolean,
+        reflect: true,
+      },
+
+      /**
+       * shows Indicator at bottom depends on total items when property is true.
+       */
+      hasIndicator: {
+        type: Boolean,
+        reflect: true,
+      },
+    };
+  }
+
+  constructor() {
+    super();
+    this.type = "slide";
+    this.rewind = false;
+    this.perPage = 1;
+    this.autoPlay = false;
+    this.interval = 5000;
+  }
+
   render() {
     return html`
-      <div class="container">
-        <div class="track-container">${this._getContentTemplate}</div>
+      <div class="splide">
+        <div class="splide__track">
+          <ul class="splide__list">
+            ${this._getContentTemplate}
+          </ul>
+        </div>
       </div>
     `;
+  }
+
+  firstUpdated() {
+    let element = this.renderRoot.querySelector(".splide");
+    new Splide(element, {
+      type: this.type,
+      rewind: this.rewind,
+      autoplay: this.autoPlay,
+      perPage: this.perPage,
+      interval: this.interval,
+    }).mount();
   }
 
   get _getContentTemplate() {
